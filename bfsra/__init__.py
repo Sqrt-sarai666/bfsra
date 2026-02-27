08aef5ac064cef2a1454d21bdb5d998581679f12
Sarai Ramos <ra471173@uaeh.edu.mx>
2026-02-27 00:05:54 +0000

    add Knapsack_problem

--- bfsra/__init__.py
+++ bfsra/__init__.py
@@ -41,3 +41,23 @@ def is_3_colorable(graph):
             return coloring
     return False
 
+def sum_of_values(values, keys):
+    # Hace la suma de los valores del arreglo
+    n = len(values)
+    sum = 0
+    for i in range(n):
+        sum += values[i]*keys[i]
+    return sum
+
+def knapsack_problem_optimal0(profits, weights, capacity, goal):
+    #Solucion por fuerza bruta para knapsack problem
+    if len(weights) != len(profits):
+        return False
+    n = len(profits)
+    sequences = itertools.product([0,1],repeat = n)
+    for secuence in sequences:
+        if sum_of_values(weights , secuence) <= capacity:
+            if sum_of_values(profits, secuence) >= goal:
+                return secuence
+    return False
+
