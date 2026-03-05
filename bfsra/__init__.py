17f62e134c1aaa205432a89f0be563a2cfcd6787
Sarai Ramos <ra471173@uaeh.edu.mx>
2026-03-05 23:39:18 +0000

    arreglando el codigo

--- bfsra/__init__.py
+++ bfsra/__init__.py
@@ -1,3 +1,3 @@
-from Hamiltonian import is_hamiltonian, is_hamiltonian_cycle
-from Knapsack import sum_of_values, knapsack_problem
-from Three_coloreable import is_3_colorable , is_proper_coloring
+from bfsra.Hamiltonian import is_hamiltonian, is_hamiltonian_cycle
+from bfsra.Knapsack import sum_of_values, knapsack_problem
+from bfsra.Three_coloreable import is_3_colorable , is_proper_coloring
